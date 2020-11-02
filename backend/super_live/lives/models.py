9087ef0113f3506a221371e6041527285e332205
SubTaro <ryo0929okubo@gmail.com>
2020-11-02 10:16:11 +0900

    最初のモデル

--- backend/super_live/lives/models.py
+++ backend/super_live/lives/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+#liveのURLを管理
+class Live(models.Model):
+    liveURL = models.CharField(max_length=2000)
+    pub_date = models.DateFiels()
+
+    def __str__(self):
+        return self.LiveURL
+
+#リアクションの管理
+class Reaction(models.Model):
+    live = models.Foreignkey(Live, on_delete=models.CASCADE)
+    reaction = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.reaction
