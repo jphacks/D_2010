074cfeeeac97e64b1d0b130e6dc2dce6538723fb
SubTaro <ryo0929okubo@gmail.com>
2020-11-03 23:39:48 +0900

    再修正

--- backend/super_live/lives/models.py
+++ backend/super_live/lives/models.py
@@ -1,23 +1,27 @@
+import datetime
+
 from django.db import models
+from django.utils import timezone
 
 #liveのURLを管理
 class Live(models.Model):
+    liveName = models.CharField(max_length=100)
     liveId = models.CharField(max_length=100)
-    pub_date = models.DateFiels()
+    pub_date = models.DateTimeField()
 
     def __str__(self):
         return self.LiveId
 
 #リアクションの管理
 class Reactions(models.Model):
-    live = models.Foreignkey(Live, on_delete=models.CASCADE)
+    live = models.ForeignKey(Live, on_delete=models.CASCADE)
     reaction = models.CharField(max_length=100)
 
     def __str__(self):
         return self.reaction
-
+#リアクションの数を管理
 class ReactionCount(models.Model):
     happy = models.IntegerField(default=0)
 
-    def __str __(self):
+    def __str__(self):
         return self.happy
