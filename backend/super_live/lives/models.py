68f7fb1baac85c82c6671f1a7883ff563643f4aa
SubTaro <ryo0929okubo@gmail.com>
2020-11-03 21:51:33 +0900

    リアクションのデータベースの修正

--- backend/super_live/lives/models.py
+++ backend/super_live/lives/models.py
@@ -2,16 +2,22 @@ from django.db import models
 
 #liveのURLを管理
 class Live(models.Model):
-    liveURL = models.CharField(max_length=2000)
+    liveId = models.CharField(max_length=100)
     pub_date = models.DateFiels()
 
     def __str__(self):
-        return self.LiveURL
+        return self.LiveId
 
 #リアクションの管理
-class Reaction(models.Model):
+class Reactions(models.Model):
     live = models.Foreignkey(Live, on_delete=models.CASCADE)
-    reaction = models.IntegerField(default=0)
+    reaction = models.CharField(max_length=100)
 
     def __str__(self):
         return self.reaction
+
+class ReactionCount(models.Model):
+    happy = models.IntegerField(default=0)
+
+    def __str __(self):
+        return self.happy
