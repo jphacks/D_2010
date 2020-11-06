443a369be7653fe109b2851b4c134c415462438a
tayzar-v98 <tayzar@6ck.jp>
2020-11-07 06:38:03 +0900

    必要最低限の機能を追加

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,10 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div id="app">
+    <v-app>
+      <router-view/>
+    </v-app>
   </div>
-  <router-view/>
 </template>
-
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,18 +1,175 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div class="home" style="display: flex; justify-content: center;">
+    <div style="width: 650px; max-width: 100vw; margin-right: 0; margin-left: 0;">
+      <div style="display: flex; flex-direction: row; ">
+        <v-text-field v-model="query" @keydown.enter="changeVideo"></v-text-field>
+        <v-btn depressed
+               color="primary"
+               @click="changeVideo">
+          検索
+        </v-btn>
+      </div>
+
+      <div style="width: 100%; padding: 10px;">
+        <youtube :video-id="videoId" ref="youtube"></youtube>
+      </div>
+
+      <div style="display: flex; flex-direction: row; justify-content: center;">
+        <v-btn depressed
+               color="rgba(0,0,0,0.3)"
+               @click="sendReaction('happy')">
+          <img width="20" src="@/assets/happy.png">
+        </v-btn>
+        <v-btn depressed
+               color="rgba(0,0,0,0.3)"
+               @click="sendReaction('happy')">
+          <img width="20" src="@/assets/sad.png">
+        </v-btn>
+        <v-btn depressed
+               color="rgba(0,0,0,0.3)"
+               @click="sendReaction('happy')">
+          <img width="20" src="@/assets/wow.png">
+        </v-btn>
+        <v-btn depressed
+               color="rgba(0,0,0,0.3)"
+               @click="sendReaction('happy')">
+          <img width="20" src="@/assets/love.png">
+        </v-btn>
+      </div>
+    </div>
+    <transition name="fade">
+      <div v-if="isHappy" class="animation" :style="{ backgroundImage: 'url(' + require('@/assets/giphy.gif') + ')' }">
+      </div>
+    </transition>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script>
+const axios = require('axios')
 
-export default defineComponent({
+export default {
   name: 'Home',
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      videoId: "H66BgQAbwU8",
+      query: "",
+      reaction: "",
+      interval: undefined,
+      isHappy: false,
+    }
   },
-});
+  created() {
+    this.interval = setInterval(this.receiveReaction, 1000)
+  },
+  beforeDestroy() {
+    if (this.interval) {
+      clearInterval(this.interval)
+      this.interval = undefined
+    }
+  },
+  methods: {
+    playVideo() {
+      this.player.playVideo()
+    },
+    changeVideo() {
+      let temp;
+      this.getLiveid().then((response) => {
+        temp = response.data;
+        this.videoId = temp.id;
+      })
+    },
+    sendReaction(reaction) {
+      this.reaction = reaction;
+      this.setReaction()
+    },
+    receiveReaction() {
+      let temp;
+      this.getReaction().then((response) => {
+        temp = response.data;
+        if (temp.reaction === "happy") {
+          this.isHappy = true;
+        } else {
+          this.isHappy = false;
+        }
+      })
+    },
+    async getLiveid() {
+      try {
+        return await
+            axios.get("http://l9vkj.mocklab.io/search", {params: this.axiosParams})
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    async getReaction() {
+      try {
+        return await
+            axios.get("http://l9vkj.mocklab.io/getReaction", {params: this.getReactionParams})
+      } catch (error) {
+        console.error(error)
+      }
+    },
+    async setReaction() {
+      const client = axios.create({
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+      try {
+        return await await client.post("http://l9vkj.mocklab.io/setReaction", this.reactionParams);
+      } catch (error) {
+        console.error(error)
+      }
+    },
+
+  },
+  computed: {
+    axiosParams() {
+      const params = new URLSearchParams();
+      params.append('name', this.query);
+      return params;
+    },
+    reactionParams() {
+      const body = new URLSearchParams();
+      body.append('id', this.videoId);
+      body.append('reaction', this.reaction);
+      return body;
+    },
+    getReactionParams() {
+      const body = new URLSearchParams();
+      body.append('id', this.videoId);
+      return body;
+    },
+    player() {
+      return this.$refs.youtube.player
+    }
+  }
+}
 </script>
+
+<style lang="scss">
+
+iframe {
+  width: 100%;
+  max-width: 650px; /* Also helpful. Optional. */
+}
+
+.animation {
+  width: 100vw;
+  height: 100vh;
+  z-index: 300;
+  position: fixed;
+  top: 0;
+  left: 0;
+  background-size: cover;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .8s;
+}
+
+.fade-enter, .fade-leave-to /* .fade-leave-active below version 2.1.8 */
+{
+  opacity: 0;
+}
+</style>
