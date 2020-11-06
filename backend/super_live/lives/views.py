c36ae14d7f29ca61d5c77744eff82864af10977d
jotashimokawa <66017211+jotashimokawa@users.noreply.github.com>
2020-11-06 11:22:26 +0900

    Update views.py

--- backend/super_live/lives/views.py
+++ backend/super_live/lives/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+from apiclient.discovery import build
+
+DEVELOPER_KEY = "AIzaSyBvde10k7ikU_qQU5CajasqJ57AcmlTTJc" #下川穣汰のYoutube API Key
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
+
+youtube = build(YOUTUBE_API_SERVICE_NAME,YOUTUBE_API_VERSION,developerKey=DEVELOPER_KEY,cache_discovery=False)
+response = youtube.videos().list(part="snippet,liveStreamingDetails",id="SORD03t7nlo").execute()　#idの部分はhttps://www.youtube.com/watch?v='hogehoge' <-ここ！
+#print(response) 辞書型で動画のsnippetとliveStreamingDetailの情報が見れる
+print(response["items"][0]["liveStreamingDetails"]["concurrentViewers"])
